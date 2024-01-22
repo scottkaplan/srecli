08de3b6d4b7bb1eb14b16e5fd7c22e924f632f6e
Scott Kaplan <scott@kaplans.com>
2024-01-21 18:38:54 -0800

    Implement list function.

--- cmd/list.go
+++ cmd/list.go
@@ -1,9 +1,16 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	// "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 var listCmd = &cobra.Command{
@@ -16,9 +23,36 @@ var listCmd = &cobra.Command{
   sre list --namespace scott: List pods in namespace scott
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
 		namespace, _ := cmd.Flags().GetString("namespace")
-		fmt.Println("namespace="+namespace)
+
+		kubeconfig := "/home/scott/.kube/config"
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// create the clientset
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+		for _, pod := range pods.Items {
+			if pod.Namespace == namespace {
+				fmt.Fprintln(w, pod.Name+"\t"+pod.Namespace+"\t")
+			}
+			// fmt.Printf("pod name: %s\n", pod.Name)
+			// fmt.Printf("pod namespace: %s\n", pod.Namespace)
+			// fmt.Printf("pod phase: %#v\n", pod.Status.Phase)
+			// for key, val := range pod.Status {
+			// fmt.Printf("pod status: %s: %s\n", key, val)
+		}
+		w.Flush()
 	},
 }
 
